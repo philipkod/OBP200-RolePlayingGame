3cd568f2c40cdd26a207e4a87468fbbce5869859
philipkod <philip.andreasson0426@gmail.com>
2026-05-10 12:23:04 +0200

    Added Character class

---
+++ OBP200-RolePlayingGame/CharacterClass.cs
@@ -0,0 +1,26 @@
+﻿namespace OBP200_RolePlayingGame;
+
+public abstract class CharacterClass
+{
+    public abstract string Name { get; }
+
+    public abstract int StartMaxHp { get; }
+    public abstract int StartAttack { get; }
+    public abstract int StartDefense { get; }
+    public abstract int StartGold { get; }
+    public abstract int StartPotions { get; }
+
+    public abstract int LevelUpMaxHpBonus { get; }
+    public abstract int LevelUpAttackBonus { get; }
+    public abstract int LevelUpDefenseBonus { get; }
+
+    public abstract int CalculateDamage(int attack, int enemyDefense, Random rng);
+
+    public abstract int UseSpecial(Player player, int enemyDefense, bool vsBoss, Random rng);
+
+    public virtual double RunAwayChance => 0.25;
+
+    
+    
+    
+}
